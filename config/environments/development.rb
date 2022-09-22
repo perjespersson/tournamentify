c2fcc9c2a957ed33b4008ddde77e4986bcf8fd35
Jesper Persson <jesper.persson_96@hotmail.com>
2022-09-22 10:11:35 +0200

    added confirmation

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -69,4 +69,7 @@ Rails.application.configure do
   # config.action_cable.disable_request_forgery_protection = true
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = { :address => "localhost", :port => 1028 }
+  config.action_mailer.perform_deliveries = true
 end
