af11d8af64f12c49d39ac392bbbb2589ba4e01ab
Jesper Persson <jesper.persson_96@hotmail.com>
2022-10-01 19:57:11 +0200

    to many fixes in one commit..

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,27 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :confirmable
+  devise :database_authenticatable,
+         :registerable,
+         :recoverable,
+         :rememberable,
+         :validatable,
+         :confirmable
+
+  validates :email,
+            uniqueness: true,
+            presence: true
+  validates :username,
+            uniqueness: true,
+            presence: true
+
+  attr_accessor :login
+
+  def self.find_for_database_authentication warden_condition
+    conditions = warden_condition.dup
+    login = conditions.delete(:login)
+    where(conditions).where(
+      ["lower(username) = :value OR lower(email) = :value",
+      { value: login.strip.downcase}]).first
+  end
 end
