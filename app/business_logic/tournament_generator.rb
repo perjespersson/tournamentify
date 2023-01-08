7d1fc5eb0f60e41970b2e9b6d473a11d3871d2b9
Jesper Persson <jesper.persson_96@hotmail.com>
2023-01-08 15:45:53 +0100

    added tournament generator class

---
+++ app/business_logic/tournament_generator.rb
@@ -0,0 +1,42 @@
+class TournamentGenerator
+  def initialize(users)
+    @users = users.count.even? ? users : users << nil
+  end
+
+  def generate_games
+    games = []
+    number_of_games_per_user.times do |round|
+      # Split array into half
+      split_users = @users.each_slice(games_per_round).to_a
+
+      games_per_round.times do |user|
+        home_team = split_users[0][user]
+        away_team = split_users[1].reverse[user]
+
+        # If we have added nil, those matches shouldn't count
+        next if home_team.nil? || away_team.nil?
+        games << [home_team, away_team, (round + 1)]
+      end
+      rotate_users
+    end
+
+    games
+  end
+
+  private
+
+  # Everyone has to play against each other once
+  def number_of_games_per_user
+    @users.count - 1
+  end
+
+  # Since @users are never odd, every team should have a game every round
+  def games_per_round
+    @users.count / 2
+  end
+
+  # Move index 1 to last in the array
+  def rotate_users
+    @users.push @users.delete_at(1)
+  end
+end
--- app/controllers/tournaments_controller.rb
+++ app/controllers/tournaments_controller.rb
@@ -4,33 +4,6 @@ class TournamentsController < ApplicationController
   end
 
   def create
-    puts "================"
-    puts params[:user_ids].reject(&:empty?)
-    puts "================"
-    create_games(params[:user_ids].reject(&:empty?))
-  end
-
-  private
-
-  def create_games(users)
-    users.push nil if users.count.odd?
-
-    number_of_rounds = users.count - 1
-    number_per_groups = users.count / 2
-
-    number_of_rounds.times {
-      split_users = users.each_slice(number_per_groups).to_a
-
-      number_per_groups.times do |index|
-        next if split_users[0][index].nil? || split_users[1].reverse[index].nil?
-
-        puts split_users[0][index].to_s + " - " + split_users[1].reverse[index].to_s
-      end
-
-      puts
-
-      # Rotate array
-      users.push users.delete_at(1)
-    }
+    puts TournamentGenerator.new(params[:user_ids].reject(&:empty?)).generate_games.inspect
   end
 end
